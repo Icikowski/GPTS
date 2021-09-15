9a1d3f7658747e96e4148fa9462dc521fc828ff0
Piotr Icikowski <piotrekicikowski@gmail.com>
2021-09-15 15:06:58 +0200

    Change default healtcheck port to 8081

--- application/common/env.go
+++ application/common/env.go
@@ -14,8 +14,8 @@ var (
 	// ServerPort determines the port number on which service will be running (defaults to 80)
 	ServerPort = getFromEnvironment("GPTS_SERVER_PORT", "80")
 
-	// HealthcheckPort determines the port number on which liveness & readiness endpoints will be running (defaults to 8000)
-	HealthcheckPort = getFromEnvironment("GPTS_HEALTHCHECK_PORT", "8000")
+	// HealthcheckPort determines the port number on which liveness & readiness endpoints will be running (defaults to 8081)
+	HealthcheckPort = getFromEnvironment("GPTS_HEALTHCHECK_PORT", "8081")
 
 	// DefaultConfigOnStartup determines if default config should be loaded when application starts
 	DefaultConfigOnStartup = getFromEnvironment("GPTS_DEFAULT_CONFIG_ON_STARTUP", "false")
